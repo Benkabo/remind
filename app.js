ff1d050c8f18acf04786e3d5f7814ed944989e01
Benedict <42405855+Benkabo@users.noreply.github.com>
2020-12-27 15:39:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-require('dotenv').config()
+const server = require('./initialiser/http')
 
-const { createServer } = require('http')
-
-const PORT = process.env.PORT || 8080;
-
-const server = createServer(( request, response ) => {
- return response.end('Look! this is a response')
-})
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT} `)
-})
+server()
